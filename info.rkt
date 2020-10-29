de59ea9f0ab1aecdfafc680ac618ee196d29003e
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-10-30 02:07:36 +0300

    Initial Commit
    * info.rkt: New file. Contains info about pkg.
    * main.rkt: New file. Expose my-custom-hello-world function.

---
+++ info.rkt
@@ -0,0 +1,16 @@
+#lang setup/infotab
+
+(define name "hello-world")
+
+(define blurb
+  (list '(p "Hello world test package")))
+
+(define primary-file "main.rkt")
+
+(define categories '(net))
+
+(define version "0.0.0.0")
+
+(define deps '())
+
+(define build-deps '())
---
+++ main.rkt
@@ -0,0 +1,5 @@
+#lang racket
+
+(provide my-custom-hello-world)
+(define (my-custom-hello-world message)
+  (display message))
